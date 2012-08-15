5628223c46d79cc9e1c1a1745400f2433ebb6569
soone <fengyue15@163.com>
2012-08-16 00:23:04 +0800

    增加钩子接口和异常类

---
+++ core/util/hook/exception.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* @file exception.php
+* @name 钩子异常
+* @author soone fengyue15@gmail.com
+* @version 0.1
+* @date 2012-08-16
+*/
+class Webpie_Hook_Exception extends Exception{}
---
+++ core/util/hook/interface.php
@@ -0,0 +1,12 @@
+<?php
+/**
+* @file interface.php
+* @name 钩子接口
+* @author soone fengyue15@gmail.com
+* @version 0.1
+* @date 2012-08-16
+*/
+interface Webpie_Hook_Interface
+{
+	public function setup($vars);
+}
