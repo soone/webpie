255ce9a5fe76cce6df6084ba8b9e94dcea141ca0
soone <fengyue15@163.com>
2012-08-31 14:49:38 +0800

    增加dal工厂方法

--- core/module.php
+++ core/module.php
@@ -3,9 +3,18 @@ class Webpie_Module_Exception extends Webpie_Exception{}
 
 class Webpie_Module
 {
+	public $dalObj;
+	public $cObj;
+
 	public function __construct(){}
 
-	public function getByCacheAndDb($cKey, $cObj, array $dbObj)
+	public function factoryDal($dalInfo)
+	{
+		$this->dalObj = new Webpie_Dal($dalInfo);
+		$this->cObj = $this->dalObj->factory();
+	}
+
+	public function getByCacheAndDb($cKey, $cObj, array $dbObj, $timeout = NULL)
 	{
 		if(!is_object($cObj))
 			throw new Webpie_Module_Exception('No cache object');
@@ -17,6 +26,8 @@ class Webpie_Module
 		if(!is_object($dbObj[0]) || !is_callable(array($dbObj[0], $dbObj[1])))
 			throw new Webpie_Module_Exception('No model object or no callable method');
 
-		return call_user_func_array(array($dbObj[0], $dbObj[1]), !empty($dbObj[2]) ? $dbObj[2] : array());
+		$cInfo = call_user_func_array(array($dbObj[0], $dbObj[1]), !empty($dbObj[2]) ? $dbObj[2] : array());
+		$cObj->set($cKey, $cInfo, $timeout);
+		return $cInfo;
 	}
 }
